134f60aeac1833895ec1fbca18a37a76f8c8a707
kumar ravi <hi@kumarravi.me>
2019-03-06 15:35:39 +0530

    update social link and offical link of creator
    Signed-off-by: kumar ravi <hi@kumarravi.me>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,9 +35,10 @@
 	
 	<div class="bg-img1 size1 flex-w flex-c-m p-t-55 p-b-55 p-l-15 p-r-15" style="background-image: url('images/bg01.jpg');">
 		<div class="wsize1 bor1 bg1 p-t-175 p-b-45 p-l-15 p-r-15 respon1">
-			<p class="txt-center m1-txt1 p-t-33 p-b-68">
-				Laracon will start in
-			</p>
+			<p class="txt-center m1-txt1">
+				Watch laracon live here: <a href="https://laracon.net/">Laracon Online Official</a>
+            </p>
+            <p class="txt-center m1-txt1 p-t-20 p-b-68">Laracon will start in</p>
 
 			<div class="wsize2 flex-w flex-c hsize1 cd100">
 				<div class="flex-col-c-m size2 how-countdown">
@@ -53,8 +54,27 @@
 				<div class="flex-col-c-m size2 how-countdown">
 					<span class="l1-txt1 p-b-9 seconds">39</span>
 					<span class="s1-txt1">Seconds</span>
-				</div>
-			</div>			
+                </div>
+            </div>	
+            <div class="row flex-c-m p-t-55">
+                <a href="https://twitter.com/kumarravi_me" target="_blank" class="flex-c-m s1-txt3 size3 how-btn trans-04 where1">
+                    <i class="fa fa-twitter">
+                        Twitter
+                    </i>
+				</a>
+                <a href="https://www.linkedin.com/in/kumarravisingh/" target="_blank" class="flex-c-m s1-txt3 size3 how-btn trans-04 where1">
+                    <i class="fa fa-linkedin">
+                        Linkeding
+                    </i>
+                </a>
+                <a href="https://github.com/kumarravisingh" target="_blank" class="flex-c-m s1-txt3 size3 how-btn trans-04 where1">
+                    <i class="fa fa-github">
+                        Github
+                    </i>
+                </a>
+                
+				
+            </div>		
 		</div>
 	</div>
 
